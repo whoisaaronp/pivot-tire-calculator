3bed35a9d097f98a03f00cea79c7c859a153aa46
whoisaaronp <juniha.aaron@gmail.com>
2021-02-23 09:46:00 -0500

    creating schema file

---
+++ models/schema.js
@@ -0,0 +1,50 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+export const OptimalSchema = new Schema({
+    userName: {
+        type: String,
+        required: 'Enter a first name'
+    },
+    passWord:{
+        type: Number,
+        required: 'Enter numbers 1 to 4'
+    },
+    massMeasurement:{
+        type: String,
+        required: 'Select a unit of mass'
+    },
+    pressureMeasurement:{
+        type: String,
+        required: 'Select a unit of pressure'
+    },
+    ridingStyle:{
+        type: String,
+        required: 'Select a bicycle type'
+    },
+    riderWeight:{
+        type: integer,
+        required: 'Select your weight'
+    },
+    rimWidth:{
+        type: integer,
+        required: 'Select your rim width'
+    },
+    innerRimWidth:{
+        type: integer,
+        required: 'Select your inner rim width'
+    },
+    wheelDiameter:{
+        type: String,
+        required: 'Select a unit of mass'
+    },
+    rimType:{
+        type: String,
+        required: 'Select your rim holding type'
+    },
+    created_date:{
+        type: Date,
+        default: Date.now
+    }
+
+});
--- public/js/localStorage.js
+++ public/js/localStorage.js
@@ -1,3 +1,13 @@
 // JavaScript Document
 
 console.log("local storage linked");
+
+const form = document.querySelector("#mass-measurement");
+const next = document.querySelector("#next-page");
+
+function getData() {
+  console.log(form.elements.mass.value);
+  //code to write to local storage goes here.
+}
+
+next.addEventListener("click", getData);
