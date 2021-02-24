9c07ff54af6c42eb47f92fa5ee5ed42ef2c614c3
whoisaaronp <juniha.aaron@gmail.com>
2021-02-24 14:44:43 -0500

    solved input radio buttons

--- public/js/localStorage.js
+++ public/js/localStorage.js
@@ -17,3 +17,8 @@ function getData() {
 }
 
 next.addEventListener("click", getData);
+
+// local storage job don't first then -> redirect
+// remove the a tag from html, redirect through JS
+// data attrituve on the continue button -> redirect to that page
+// localstorage can work on each page 
