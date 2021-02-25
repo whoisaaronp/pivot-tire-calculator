1d0dc3aaf4bf4a35c8c96734dbd2fe6e0a979012
whoisaaronp <juniha.aaron@gmail.com>
2021-02-24 23:09:58 -0500

    local storage functions features

--- public/js/localStorage.js
+++ public/js/localStorage.js
@@ -2,24 +2,72 @@
 
 console.log("local storage linked");
 
-const form = document.querySelector("#mass-measurement");
-// next is continue button-> when click do this…
+const form = document.querySelector("form");
 const next = document.querySelector("#next-page");
 
+// 1. selected buttons function
 function getData() {
-  console.log(form.elements.mass.value);
-  //code to write to local storage goes here.
-  // collecting, storing and submitting alax call
-  let kg = "kg";
-  let str = JSON.stringify(kg);
-  localStorage.setItem("KG", kg);
+  // console.log(form.elements.mass.value);
+  const selectedRadio = form.querySelector('input[type="radio"]:checked');
+  //change to range, for the sliders
+  console.log(selectedRadio);
 
+  if (selectedRadio) {
+    alert('saved!');
+    const value = selectedRadio.getAttribute('value');
+    const propertyName = selectedRadio.getAttribute('name');
+    let str = JSON.stringify(value);
+    localStorage.setItem(propertyName, str);
+  }
 }
 
-next.addEventListener("click", getData);
+// 2. selected range function
+function getData() {
+  // console.log(form.elements.mass.value);
+  const selectedRange = form.querySelector('input[type="range"]');
+  //change to range, for the sliders
+  console.log(selectedRange);
 
-// local storage job done first then -> redirect
-// remove the a tag from html pages, redirect through JS instead
-// data attritube on the continue button -> redirect to that page instead
-// localstorage can work on each page 
-// window localStorage
+  if (selectedRadio) {
+    alert('saved!');
+    const value = selectedRange.getAttribute('value');
+    const propertyName = selectedRange.getAttribute('name');
+    let str = JSON.stringify(value);
+    localStorage.setItem(propertyName, str);
+  }
+}
+
+// 3. Bike functions
+const saveBikeBtn = document.getElementById('saveBikeBtn');
+if (saveBikeBtn) {
+  saveBikeBtn.addEventListener('click', saveBike);
+}
+
+function saveBike() {
+  const bikeData = [
+    "Mountain Bike",
+    "Cyclocross",
+    "Road Bike",
+    "Hydrid",
+    "Active"
+
+  ];
+
+  const selectedBike = bikeData[swiper.activeIndex];
+  localStorage.setItem('bike', selectedBike);
+}
+
+// next.addEventListener("click", getData);
+// On click event function
+const linkBtns = document.querySelectorAll('[data-save-then-goto]');
+
+linkBtns.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    const href = e.target.getAttribute('data-save-then-goto');
+
+    getData();
+
+    window.location.href = href;
+
+  });
+});
