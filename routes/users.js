1625837f2e4576c5c0256c308cd851ae4bc3a90c
whoisaaronp <juniha.aaron@gmail.com>
2021-03-31 13:13:00 -0400

    writing desciption for each page

--- routes/users.js
+++ routes/users.js
@@ -7,25 +7,25 @@ mongoose.set('useUnifiedTopology', true);
 mongoose.set('useNewUrlParser', true);
 
 // intailizing mongo at the start …
-mongoose.connect('mongodb+srv://apaterson:Fq3FIdFYIuICGyj7@capstone.5wnns.mongodb.net/tirecalculatorDB?retryWrites=true&w=majority', function(err) {
-  if(err) {
+mongoose.connect('mongodb+srv://apaterson:Fq3FIdFYIuICGyj7@capstone.5wnns.mongodb.net/tirecalculatorDB?retryWrites=true&w=majority', function (err) {
+  if (err) {
     console.log('error connecting', err);
-  }else{
+  } else {
     console.log('connected');
   }
-  });
+});
 
 // call back function name, query, call back… to hold to data 
-function find (name, query, cb) {
+function find(name, query, cb) {
   mongoose.connection.db.collection(name, function (err, collection) {
-      collection.find(query).toArray(cb);
+    collection.find(query).toArray(cb);
   });
 }
 
 /* GET users listing. */
 // BACKEND
 // Received documents as an argument and sends to the browser…
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   find('users', '', (err, docs) => {
     res.send(JSON.stringify(docs));
   });
@@ -55,5 +55,8 @@ module.exports = router;
 // let frontPressure = localStorage.getItem('front_pressure'),
 // let rearPressure = localStorage.getItem('rear_pressure')
 
-// put everything in a json object
-let myCompass = "{user:"+theUser+"tirePressure,:"+tp...
+// put everything in a json object concatitnate
+// let storeLocal = "{user:" + theUser + ",pressure-unit:" + pressureUnit + ",weight-unit:" + massUnit + ",bike:" + riderStyle + ",rider-weight:" + humanWeight + ",rim-width:" + rimWidth + ",rim-type:" + rimType + ", road-surface:" + roadSurface + ", wheel-diameter:" + wheelDiameter + ", front_pressure:" + frontPressure + ",rear_pressure:" + rearPressure + "}";
+
+// the send it
+// Mongoosemodel.insertOne(storeLocal);
