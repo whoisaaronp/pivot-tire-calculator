f062f21eced8cdc16c92bd4acdd816fc0d812d43
whoisaaronp <juniha.aaron@gmail.com>
2021-04-10 13:36:35 -0400

    added registering page or unique db entry

--- routes/index.js
+++ routes/index.js
@@ -72,6 +72,10 @@ router.get('/settings', function (req, res, next) {
     res.render('settings');
 });
 
+router.get('/register', function (req, res, next) {
+    res.render('register');
+});
+
 
 module.exports = router;
 
